cc61a757804ffd631ed4cb1775f5d75f8d8c642e
poweiiiii <4b0g0175@stust.edu.tw>
2025-03-04 14:18:05 +0800

    Add page transition

--- src/components/Page_Transition/Page_Transition.js
+++ src/components/Page_Transition/Page_Transition.js
@@ -17,17 +17,18 @@ const Page_Transition = ({ page_title, onAnimationEnd }) => {
         const timer = setTimeout(() => {
             setAnimationActive(false);
             if (onAnimationEnd) onAnimationEnd();
-        }, 1000000000);
+        }, 1800);
         return () => clearTimeout(timer);
     }, [onAnimationEnd]);
     return (
         <div>
             {animationActive && (
-                <div className="page_transition_container">
-                    <div className="transition_circle">
-                        <p className="page_title">{page_title}</p>
-                    </div>
-                </div>
+                    <div className="page_transition_container">
+                    {/* 放在 container 裡，透過絕對定位讓圓圈位於正中央 */}
+                    <div className="transition_circle"></div>
+                    {/* 頁面名稱，獨立於圓圈，不受放大影響 */}
+                    <p className="page_title">{page_title}</p>
+                  </div>
             )}
         </div>
     )
